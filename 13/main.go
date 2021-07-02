4dd8c0b0523bb9374c41db9782255d1ff4897739
Matthew Taylor <matthew.taylor@starlingbank.com>
2021-07-02 15:00:30 +0100

    wip13

--- 13/main.go
+++ 13/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"math"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -12,10 +15,39 @@ func main() {
 
 	reader := bufio.NewReader(file)
 	line, _ := reader.ReadString('\n')
-	earliestTime, _ := strconv.Atoi(line)
+	earliestTime, err := strconv.Atoi(strings.TrimSuffix(line, "\n"))
+	if err != nil {
+		panic(err)
+	}
 
 	line, _ = reader.ReadString('\n')
 	timetable := parseTimetable(line)
+
+	fmt.Printf("Earliest bus ID: %v", findEarliestBusId(timetable, earliestTime))
 }
 
-func parseTimetable(string line)
+func parseTimetable(line string) (busses []int) {
+	for _, bus := range strings.Split(line, ",") {
+		busNum, err := strconv.Atoi(bus)
+		if err == nil {
+			busses = append(busses, busNum)
+		}
+	}
+	return
+}
+
+func findEarliestBus(busses []int, earliestTime int) (id int) {
+	earliestBus := math.MaxInt32
+
+	for _, bus := range busses {
+		if earliestTime%bus == 0 {
+			id = (earliestTime / bus)
+		} else {
+			id = (earliestTime / bus) + 1
+		}
+
+		if id < earliestBus {
+			earliestBus = id
+		}
+	}
+}
---
+++ 13/main_test.go
@@ -0,0 +1,9 @@
+package main
+
+var testData = struct {
+	earliestTime int
+	timetable    string
+}{
+	939,
+	"7,13,x,x,59,x,31,19",
+}
